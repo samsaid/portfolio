4a18c71cda4ab4fd9f50dc68871ba79972981dc8
samsaid <smsaid@asu.edu>
2021-12-25 15:54:42 -0700

    add font

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/experience.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>Hey there, I'm Samira. ✰ </p>
+        <p>✹ A computer science graduate just making her way through life. <br></br>❊⋆✽⋆✷</p>
       </header>
     </div>
   );
