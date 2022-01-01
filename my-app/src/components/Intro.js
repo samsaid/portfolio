48c42570a354a0ddcbabe7692167bf71379d7557
samsaid <smsaid@asu.edu>
2022-01-01 00:20:42 -0700

    edit intro text

--- my-app/src/components/Intro.js
+++ my-app/src/components/Intro.js
@@ -4,7 +4,7 @@ function Intro() {
   return (
   <div id="intro">
     <div id='title'>hi, samira here. </div>
-    <div id='desc'> I'm a software engineer based in Arizona.
+    <div id='desc'> I'm a software engineer that enjoys collaboration and challenges.
     My intrests include full-stack development, human computer interaction, machine learning, and everything in between.</div>
   </div>
     );
