92b808279fa4bc358d1076c6b5c65c91abfb600c
samsaid <smsaid@asu.edu>
2022-01-16 00:28:09 -0700

    edit experience

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -9,12 +9,11 @@ function About(){
                     <div className='column'>
                         <h2>Education</h2>
                         <div className='desc'>
-                            <h4>Arizona State University - Ira A. Fulton Schools of Engineering </h4>
+                            <h4>Arizona State University -  Ira A. Fulton Schools of Engineering </h4>
                             <h5>Bachelor of Science, Computer Science</h5>
                             <ul>
-                                <li>Expected December 2021</li>
-                                <li>Activities: Computer Science Undergraduate Teaching Assistant, Study Abroad, Engineering Projects in Computer Science</li>
-                                <li>Organizations: National Society of Black Engineers (NSBE), Women in Computer Science (WiCS)</li>
+                                <li>Graduated December 2021</li>
+                                <li>Activities: Undergraduate Teaching Assistant, Study Abroad, Engineering Projects in Computer Science, National Society of Black Engineers (NSBE), Women in Computer Science (WiCS)</li>
                             </ul>
                         </div>
                     </div>
--- my-app/src/components/Experience.js
+++ my-app/src/components/Experience.js
@@ -14,32 +14,31 @@ function Experience(){
                             <div className='d'>
                             <p>Interned for the Aladdin Product Group (APG) Data Layer Team developing a Visual Studio Code plugin that implements syntax highlighting on a file based on database rules from an Apache Database.</p>
                             </div>
-                            <div className='skills'>
+                            
                             <ul>
                             <li>TypeScript</li>
                             <li>Node.js</li>
-                            <li>Parsing Expression Grammar</li>
-                            <li>VS Code Extension API</li>
+                            <li>VS Code</li>
                         </ul>
-                            </div>
+                            
                        
                         </div>
                         
                         
                         <h4>Software Engineer Intern</h4>
-
+                        <h5>Summer 2020</h5>
                         <div className='experience-item'>
                             <div className='d'>
                             <p>Interned for the Aladdin Product Group (APG) Active Tools team developing new features and solving bugs in tickets for an application used by internal customers.</p>
                             </div>
-                            <div className='skills'>
+                            
                             <ul>
                             <li>JavaScript</li>
+                            <li>HTML/CSS</li>
                             <li>Java</li>
                             <li>Angular</li>
-                            <li>Spring Boot</li>
                         </ul>
-                            </div>
+                            
                         </div>
 
                         <h3>Amkor Technology</h3>
@@ -49,12 +48,12 @@ function Experience(){
                             <div className='d'>
                             <p>Interned on the Web Applications team to develop a web front end for legacy native applications. Completed and maintained two projects during internship duration. </p>
                             </div>
-                            <div className='skills'>
+                            
                             <ul>
                             <li>JavaScript</li>
                             <li>HTML/CSS</li>
                         </ul>
-                            </div>
+                           
                         </div>
                         
                     </div>
