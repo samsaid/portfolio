1f0825b97a1f9be33c1b07c2276b53403bfb77bd
samsaid <smsaid@asu.edu>
2022-01-16 21:19:57 -0700

    add resume

--- my-app/src/components/Intro.js
+++ my-app/src/components/Intro.js
@@ -16,9 +16,11 @@ function Intro() {
             <h1>Samira Said</h1>
             </div>     
             <div id='link'>
-              <a href="https://www.github.com/samsaid" id="link"><i class="fa fa-github-square"></i></a>
+              <a href="https://www.github.com/samsaid" id="link"> <i class="fa fa-github-square"></i></a> 
               &nbsp;&nbsp;
               <a href="https://www.linkedin.com/in/smsaid/" id="link"><i class="fa fa-linkedin"></i></a> 
+              &nbsp;&nbsp;
+              <a href="https://drive.google.com/file/d/1a9TCdv7qMr0-S3X6FC7F4uOKlP8gSJng/view?usp=sharing" id="link"><i class="fa fa-paperclip"></i></a> 
             </div>
             
         </div>
