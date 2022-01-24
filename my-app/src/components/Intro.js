d743b9515236e7e253f713e2e469a61c9d7ab4a1
samsaid <smsaid@asu.edu>
2022-01-24 11:40:07 -0700

    update resume

--- my-app/src/components/Intro.js
+++ my-app/src/components/Intro.js
@@ -1,39 +1,39 @@
-import '../styles/Intro.css'
+import "../styles/Intro.css";
 
 function Intro() {
   var image = require("../images/sam1.jpg");
 
   return (
-  
-    <div className='container'> 
-      <div className='row'>
-      
-        <div className='intro-text'>
-        
+    <div className="container">
+      <div className="row">
+        <div className="intro-text">
           <div id="title">
             <h1>Samira Said</h1>
-            </div>     
-            <div id='link'>
-              <a href="https://www.github.com/samsaid" id="link"> <i className="fa fa-github-square"></i></a> 
-              &nbsp;&nbsp;
-              <a href="https://www.linkedin.com/in/smsaid/" id="link"><i className="fa fa-linkedin"></i></a> 
-            </div>
+          </div>
+          <div id="link">
+            <a href="https://www.github.com/samsaid" id="link">
+              {" "}
+              <i className="fa fa-github-square"></i>
+            </a>
+            &nbsp;&nbsp;
+            <a href="https://www.linkedin.com/in/smsaid/" id="link">
+              <i className="fa fa-linkedin"></i>
+            </a>
+          </div>
           <div>
-             <br></br>
-             <div id="button-space">
-            <form action="https://drive.google.com/file/d/1BUWjRHGLTVgvWVLkWIKUr9Qhi7Bn_UIl/view?usp=sharing">
-            <input id="btn" type="submit" value="Resume as PDF" />
-            </form>
+            <br></br>
+            <div id="button-space">
+              <form action="https://drive.google.com/file/d/147xxVE86JEuwR_w4fQqCJn5woBsaz0Eq/view?usp=sharing">
+                <input id="btn" type="submit" value="Resume as PDF" />
+              </form>
             </div>
-            </div>
+          </div>
         </div>
-        <div className='about-image'>
-          <img className='profile-img' src={image}></img>
+        <div className="about-image">
+          <img className="profile-img" src={image}></img>
         </div>
-        
-        </div>    
+      </div>
     </div>
-  
-    );
+  );
 }
 export default Intro;
