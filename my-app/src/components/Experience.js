6b1aa136d818d16fb6e29da0d652c27b1b4499e3
samsaid <smsaid@asu.edu>
2022-04-27 14:55:00 -0700

    css changes

--- my-app/src/components/Experience.js
+++ my-app/src/components/Experience.js
@@ -10,13 +10,14 @@ function Experience(){
                     <div className='desc'>
                     <h3>BlackRock</h3>
                         <h4>Sofware Engineer Intern </h4>
-                        <h5>Summer 2021</h5>
                         <div className='experience-item'>
                             <div className='d'>
+                            <p><i>Summer 2021</i></p>
                             <p>Interned for the Aladdin Product Group (APG) Data Layer Team developing a Visual 
-                                Studio Code plugin to improve developer productivity.</p>
+                                Studio Code plugin for developer productivity.</p>
                             </div>
                             <div className='skills'>
+                            <p id='s'><b>Skills</b></p>
                             <ul>
                             <li>TypeScript</li>
                             <li>Node.js</li>
@@ -28,12 +29,14 @@ function Experience(){
                         
                         <h3>BlackRock</h3>
                         <h4>Software Engineer Intern</h4>
-                        <h5>Summer 2020</h5>
+                        
                         <div className='experience-item'>
                             <div className='d'>
+                            <p><i>Summer 2020</i></p>
                             <p>Interned for the Aladdin Product Group (APG) Active Tools Team developing new features and solving bugs in tickets for an Angular application used by internal customers.</p>
                             </div>
                             <div className='skills'>
+                            <p id='s'><b>Skills</b></p>
                             <ul>
                             <li>JavaScript</li>
                             <li>HTML/CSS</li>
@@ -47,12 +50,13 @@ function Experience(){
                         </div>
                         <h3>Amkor Technology</h3>
                         <h4>Web Applications Intern/Co-op</h4>
-                        <h5>Summer 2019 - Spring 2020</h5>
                         <div className='experience-item'>
                             <div className='d'>
+                                <p><i>Summer 2019 - Spring 2020</i></p>
                             <p>Interned on the Web Applications Team and completed two projects developing a web front end for native applications.  </p>
                             </div>
                             <div className='skills'>
+                            <p id='s'><b>Skills</b></p>
                             <ul>
                             <li>JavaScript</li>
                             <li>HTML/CSS</li>
