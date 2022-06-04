d7dd262f9467661bf10297b7aa1fc88896ef00ab
samsaid <smsaid@asu.edu>
2022-06-04 13:51:58 -0700

    adding coursework

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -11,6 +11,8 @@ function About(){
                             <ul>
                                 <li>B.S. Computer Science </li>
                                 <li>Graduated December 2021</li>
+                                <li><b>Leadership/Organizations:</b> Undergraduate Teaching Assistant, National Society of Black Engineers, Women in Computer Science</li>
+                                <li><b>Courses:</b> Operating Systems, Design & Analysis of Algorithms, Mobile Application Development, Probability and Statistics, Database Management, Data Visualization, Multimedia Information Systems</li>
                             </ul>
                         </div>
                     </div>
