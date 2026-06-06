4a02f195ca7b70a4d7f6549fdfcbb2a362560851
Samira Said <smsaid@asu.edu>
2026-06-06 00:29:35 -0700

    fetch date dynamically from github

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,11 +1,10 @@
 import { useState, useEffect } from 'react';
 import '../styles/Footer.css'
 
-const LAST_UPDATED = '09-30-25';
-
 function Footer(){
     const [time, setTime] = useState('');
     const [emoji, setEmoji] = useState('');
+    const [lastUpdated, setLastUpdated] = useState('');
 
     useEffect(() => {
         const update = () => {
@@ -28,13 +27,28 @@ function Footer(){
         return () => clearInterval(interval);
     }, []);
 
+    useEffect(() => {
+        fetch('https://api.github.com/repos/samsaid/portfolio/commits/new-format')
+            .then(res => res.json())
+            .then(data => {
+                const date = new Date(data.commit.committer.date);
+                const formatted = date.toLocaleDateString('en-US', {
+                    month: '2-digit',
+                    day: '2-digit',
+                    year: '2-digit',
+                }).replace(/\//g, '-');
+                setLastUpdated(formatted);
+            })
+            .catch(() => setLastUpdated(''));
+    }, []);
+
     return(
         <footer id='footer'>
             <div className="footer-row">
                 <span className="footer-location">{emoji} {time}, Phoenix</span>
             </div>
             <div className="footer-row">
-                <a className="changelog" href="https://github.com/samsaid/portfolio" target="_blank" rel="noopener noreferrer">last updated {LAST_UPDATED}</a>
+                <a className="changelog" href="https://github.com/samsaid/portfolio" target="_blank" rel="noopener noreferrer">last updated {lastUpdated}</a>
             </div>
         </footer>
     );
--- my-app/src/components/Intro.js
+++ my-app/src/components/Intro.js
@@ -1,7 +1,7 @@
 import "../styles/Intro.css";
 
 function Intro() {
-  var image = require("../images/sam3.jpg");
+  // var image = require("../images/sam3.jpg");
 
   return (
     <div className="intro-container">
